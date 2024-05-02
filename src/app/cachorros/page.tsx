e62a6f5176672af3eac979fd36c0a74e1dbcf8b4
juliocesardemoraes <jcmcf2012@gmail.com>
2024-05-02 10:46:45 -0300

    adicionando paginas e header

---
+++ src/app/cachorros/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/comps/Header/Header";
+import React from "react";
+
+export default function page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center  pl-24 pt-5 pr-24">
+      <Header></Header>
+    </main>
+  );
+}
